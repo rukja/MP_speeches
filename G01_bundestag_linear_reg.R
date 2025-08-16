df86fd1f4292fbbca38442729a858486b30f2496
rukja <orukja@gmail.com>
2025-08-16 12:16:08 -0700

    Create linear regression file for bundestag

---
+++ G01_bundestag_linear_reg.R
@@ -0,0 +1,17 @@
+# Here, I perform linear regression analysis for exploring the relationship between
+# mpds, sex, party, and sentiment
+
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(purrr)
+library(qs)
+library(reshape2)
+
+# Load external and related packages
+
+
+# Read in data =====
+df.merged.mpds <- qread("bundestag_mpds_gender_sentiment_df.qs")
+df.merged <- qread("bundestag_gender_sentiment_df.qs")
