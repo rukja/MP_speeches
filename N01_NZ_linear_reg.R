c2244615088d2791a165ce1a72699c85a9b82ea7
rukja <orukja@gmail.com>
2025-08-22 10:07:22 -0700

    Created NZ linear reg

---
+++ N01_NZ_linear_reg.R
@@ -0,0 +1,73 @@
+# Here, I perform linear regression analysis for exploring the relationship between
+#sex, Party, and sentiment
+
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(purrr)
+library(qs)
+library(reshape2)
+library(car)
+
+# Load external and related packages
+
+# Leaps
+library(leaps)
+library(robustbase) # For robust regression
+library(emmeans)
+
+
+# Read in data =====
+df.merged <- qread("nz_files/nz_gender_sentiment_df_status.qs")
+
+
+# Linear regression has assumptions ======
+
+hist(df.merged$sentiment)
+
+qqPlot(df.merged$sentiment)
+
+ks.test(df.merged$sentiment, "pnorm", 1, 2) 
+
+summary(df.merged$sentiment)
+
+#=====# Perform analysis without mpds variables =================
+
+## Data is not normally distributed; sample size is large, however
+## We use robust regression  =======
+
+df.merged$Party <- as.factor(df.merged$Party)
+df.merged$gender <- as.factor(df.merged$gender)
+df.merged$year <- as.factor(df.merged$Year)
+df.merged$Status <- as.factor(df.merged$Status)
+
+df.merged$Party  <- relevel(df.merged$Party, ref = "PDS/LINKE")
+df.merged$Status <- relevel(df.merged$Status, ref = "opp")
+df.merged$gender <- relevel(df.merged$gender, ref = "male")
+
+form <- as.numeric(sentiment) ~ Party + chair:Party + gender + gender:Party + Status + Party:Status + chair + gender:Status
+
+
+robust_model <- glmrob(form, family = "gaussian", data = df.merged)
+summary(robust_model)
+robust_residuals <- residuals(robust_model)
+
+## I want to compare with glm, and with lm ======
+
+
+basic_model <- lm(form, data = df.merged)
+summary(basic_model)
+basic_residuals <- residuals(basic_model)
+
+general_model <- glm(form, data = df.merged)
+summary(general_model)
+general_residuals <- residuals(general_model)
+
+par(mfrow=c(2,2))
+
+plot(general_model)
+
+# The model does not fit well 
+
+
