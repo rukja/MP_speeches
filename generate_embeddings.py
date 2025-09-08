c17ae1cdbc837e39b9676c556d55d1f03129d8e8
rukja <orukja@gmail.com>
2025-09-08 11:17:14 -0700

    Commit new mamba

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -20,8 +20,38 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sentence_transformers import SentenceTransformer
 from collections import defaultdict, Counter
+from spacy_cleaner import processing, Cleaner
 
 # seeds for reproducibility
 RANDOM_SEED = 19
 np.random.seed(RANDOM_SEED)
 random.seed(RANDOM_SEED)
+
+# directory setup
+output_dir = "embedding_outputs"
+os.makedirs(output_dir, exist_ok=True)
+dirs = {
+    'embeddings': os.path.join(output_dir, 'embeddings'),
+    'plots': os.path.join(output_dir, 'plots'),
+    'data': os.path.join(output_dir, 'data')
+            }
+
+########################################
+# 1. DATA LOADING & PREPROCESSING
+########################################
+# load speeches dataset
+df = pd.read_csv("./nz_files/nz_filtered.csv")
+
+# clean text 
+## using spacy and spacy_cleaner
+
+model_cleanse = spacy.load("en_core_web_sm")
+pipeline = spacy_cleaner.Pipeline(
+    model = model_cleanse,
+    removers.remove_stopward_token,
+    replacers.replace_punctuation_token,
+    mutators.mutate_lemma_token,
+)
+def sanitize_text(speech_column):
+
+
